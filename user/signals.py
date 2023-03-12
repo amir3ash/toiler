92679dacbf1964f2192505102b6ec3e617daa1f3
Amir Shams <amir3.ash@gmail.com>
2023-03-12 20:38:39 +0330

    stoping publishing event when updating last_login field

--- user/signals.py
+++ user/signals.py
@@ -14,5 +14,8 @@ def activity_post_delete_handler(instance: User, **kwargs):
 
 
 @receiver(post_save, sender=User, dispatch_uid='user_updated')
-def activity_post_save_handler(instance: User, created, **kwargs):
+def activity_post_save_handler(instance: User, created, update_fields, **kwargs):
+    if update_fields and len(update_fields) == 1 and 'last_login' in update_fields:
+        return  # when the user just logged in
+
     notify("added" if created else "updated", "user", instance.id, 0)
